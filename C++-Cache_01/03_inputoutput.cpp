feb3f28dcd5cd7687a1d1a6bc813b6dc066d68e2
Timo Abend <timo.abend@liquidmotions.de>
2014-01-08 08:22:51 +0100

    C++-Cache Lesson 1 added

---
+++ C++-Cache_01/03_inputoutput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cout << "Gib die erste Zahl ein: ";
+	cin >> a;
+	cout << "Gib die zweite Zahl ein: ";
+	cin >> b;
+	cout << a << " + " << b << " = " << a + b << endl;
+	return 0;
+}
