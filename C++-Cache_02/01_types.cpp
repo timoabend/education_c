fa8a283013f62feee3540a017444866110d861c4
Timo Abend <timo.abend@liquidmotions.de>
2014-01-08 08:29:40 +0100

    C++-Cache Lesson 2 added

---
+++ C++-Cache_02/01_types.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int ganzeZahl = 5;
+	float fliesskommaZahl = 5.5;
+	string gruss = "Hallo Welt";
+	char satzzeichen = '!';
+	bool isWahr = false;
+	
+	cout << ganzeZahl + fliesskommaZahl << endl;
+	cout << gruss + satzzeichen << endl;
+	cout << isWahr << endl;
+	return 0;
+}
